899ce1d5ed5ec15b74869f86905c6ef54d682f93
Eslim Daga <esliim9811@gmail.com>
2021-03-10 15:09:25 -0500

    Init basic configuration index

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+
+//Initializations
+const app = express();
+
+//Settings
+app.set("port", process.env.PORT || 4000);
+
+//Middlewares
+app.use(morgan("dev"));
+
+//Global Variables
+
+//Routes
+
+//Public
+
+//
